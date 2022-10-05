629a828d6deb5305b32b197a582d10aad459fda0
seu nome <adnanbezerra@gmail.com>
2022-10-05 17:53:42 -0300

    style: change the book icon

--- src/components/templates/Header/Header.jsx
+++ src/components/templates/Header/Header.jsx
@@ -2,14 +2,14 @@ import { Container, Title } from "./HeaderStyles";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineSearch } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { BiBookOpen } from "react-icons/bi";
+import { BsBook } from "react-icons/bs";
 
 export function Header() {
     return (
         <Container>
             <FaBars style={{ color: "#fff", fontSize: "20px" }} />
 
-            <Title><BiBookOpen style={{ marginRight: "5px" }} />TeachMe</Title>
+            <Title><BsBook style={{ marginRight: "5px" }} />TeachMe</Title>
 
             <Link to={"/search"}><div>
                 <AiOutlineSearch style={{ color: "#fff", fontSize: "30px" }} />
