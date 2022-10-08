d3cef58ed5eca51b5ef3a4fb38aa29bc25463f18
seu nome <adnanbezerra@gmail.com>
2022-10-07 21:38:39 -0300

    feat: add new screens

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,9 +17,12 @@ const LazyWrapper = (Component) => (props) => (
 
 const Template = LazyWrapper(lazy(() => import("./templates/PageTemplate/PageTemplate")));
 const MainPage = LazyWrapper(lazy(() => import("./pages/MainPage/MainPage")));
+const SearchPage = LazyWrapper(lazy(() => import("./pages/SearchPage/SearchPage")));
+const Logout = LazyWrapper(lazy(() => import("./pages/LoginPage/Logout")));
+const UserPage = LazyWrapper(lazy(() => import("./pages/UserPage/UserPage")));
 
 function App() {
-  const { user, setUser } = useState();
+  const [user, setUser] = useState();
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
@@ -31,6 +34,9 @@ function App() {
           <Route path={'/login'} element={<LoginPage />} />
           <Route path={'/'} element={<Template />}>
             <Route path={'/'} element={<MainPage />} />
+            <Route path={'/search'} element={<SearchPage />} />
+            <Route path={'/logout'} element={<Logout />} />
+            <Route path={'/user/me'} element={<UserPage />} />
           </Route>
         </Routes>
       </BrowserRouter >
